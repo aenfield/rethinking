4287886555e12ad864aa1c10ff2a07e478916c14
Andrew Enfield <aenfield@gmail.com>
2016-04-06 11:42:04 -1000

    Finished ch2.

--- ch02-scratch.R
+++ ch02-scratch.R
@@ -13,3 +13,25 @@ posterior = unstd_posterior / sum(unstd_posterior)
 
 plot(p_grid, posterior, type='b', xlab='prob. of water', ylab='posterior')
 
+
+# quadratic approximation
+library(rethinking)
+
+globe.qa = map(
+  alist(
+    w ~ dbinom(9,p),
+    p ~ dunif(0,1)
+  ),
+  data=list(w=6))
+
+precis(globe.qa)
+
+# compare quadratic approx w/ analytic/exact calculation
+w = 6
+n = 9
+curve(dbeta(x, w+1, n-w+1), from=0, to=1)
+# approximation curve, first by repeating vals and then via the S4 obj from precis
+#curve(dnorm(x, 0.67, 0.16), lty=2, add=TRUE)
+globe.qa_precis = precis(globe.qa)  # doesn't yet take w and n as params
+curve(dnorm(x, globe.qa_precis@output[['Mean']], 
+               globe.qa_precis@output[['StdDev']]), lty=2, add=TRUE)
