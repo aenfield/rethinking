29a18687bf7c39377f2c2bea8540de5f3c416706
Andrew Enfield <aenfield@gmail.com>
2016-04-07 13:51:19 -1000

    interim checkin.

--- ch03-scratch.R
+++ ch03-scratch.R
@@ -32,4 +32,17 @@ prior_h = rnorm(n, sample_mu, sample_sigma)
 dens(prior_h)
 
 # grid approx
-mu.list = seq(from=)
+mu.list = seq(from=140, to=160, length.out=200)
+sigma.list = seq(from=4, to=9, length.out=200)
+post = expand.grid(mu=mu.list, sigma=sigma.list)
+post$LL = sapply(1:nrow(post), function(i) sum(dnorm(
+                d2$height,
+                mean = post$mu[i],
+                sd = post$sigma[i],
+                log = TRUE )))
+post$prod = post$LL + dnorm(post$mu, mu_mean, mu_sigma, TRUE) + 
+                      dunif(post$sigma, sigma_min, sigma_max, TRUE)
+post$prob = exp(post$prod - max(post$prod))
+
+contour_xyz(post$mu, post$sigma, post$prob)
+image_xyz(post$mu, post$sigma, post$prob)
